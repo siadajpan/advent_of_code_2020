854c685396bd93a2c14e1969c2ce6a1055fb6a03
Karol Misiarz <karol.misiarz@robovision.eu>
2020-12-17 10:03:12 +0100

    Add second part

--- 17/second.py
+++ 17/second.py
@@ -4,7 +4,7 @@ from utils.utils import read_input
 
 
 def find_neighbours(cut):
-    neighbours = np.sum(cut) - cut[1, 1, 1]
+    neighbours = np.sum(cut) - cut[1, 1, 1, 1]
     # if neighbours:
     #     print(cut, '\nfound:', neighbours)
     return neighbours
@@ -12,18 +12,19 @@ def find_neighbours(cut):
 
 if __name__ == '__main__':
     sim_length = 7
-    data = read_input()
+    data = read_input('input')
     old_size = len(data)
     data = np.array(
         [list(d.replace('.', '0').replace('#', '1')) for d in data], dtype=int)
     old_h, old_w = data.shape
     new_size = np.array(data.shape) + sim_length * 2
     new_mid = np.zeros(new_size, dtype=int)
-    new_mid[sim_length: sim_length + old_w,
-    sim_length: sim_length + old_h] = data
-    space = np.zeros((sim_length * 2 + 1, *new_size), dtype=int)
-    space[sim_length] = new_mid
-    xm, ym, zm = space.shape
+    new_mid[sim_length: sim_length + old_w, sim_length: sim_length + old_h] = \
+        data
+    new_dim = sim_length * 2 + 1
+    space = np.zeros((new_dim, new_dim, *new_size), dtype=int)
+    space[sim_length, sim_length] = new_mid
+    xm, ym, zm, wm = space.shape
     # print(new_mid)
     # print(space)
 
@@ -32,12 +33,14 @@ if __name__ == '__main__':
         for x in range(1, xm - 1):
             for y in range(1, ym - 1):
                 for z in range(1, zm - 1):
-                    cut = new_space[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2]
-                    if new_space[x, y, z] and not find_neighbours(cut) in [2, 3]:
-                        space[x, y, z] = 0
-                    if not new_space[x, y, z] and find_neighbours(cut) == 3:
-                        # print('x y z', x, y, z)
-                        space[x, y, z] = 1
+                    for w in range(1, wm - 1):
+                        cut = new_space[x - 1:x + 2, y - 1:y + 2,
+                              z - 1:z + 2, w - 1: w + 2]
+                        if new_space[x, y, z, w] and not find_neighbours(cut) in [2, 3]:
+                            space[x, y, z, w] = 0
+                        if not new_space[x, y, z, w] and find_neighbours(cut) == 3:
+                            # print('x y z', x, y, z)
+                            space[x, y, z, w] = 1
         print('--------------\n', space)
     print(np.sum(space))
     # data[data == '.'] = 0
