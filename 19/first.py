e7f7fa4412264f0276ddd65746f9fc1c70120079
Karol <siadajpan@o2.pl>
2020-12-19 23:12:06 +0100

    Add first

--- 19/first.py
+++ 19/first.py
@@ -1,7 +1,4 @@
 import re
-from functools import lru_cache
-
-import numpy as np
 
 from utils.utils import read_input_blocks
 
@@ -10,51 +7,53 @@ def parse_rule(rule):
     key, value = re.match(r'(\d+): (.*)', rule).groups()
     only_digits = re.match('([\d+\s*]+)$', value)
     if only_digits:
-        return key, ('strict', np.array(only_digits.group(1).split(), dtype=int))
+        return key, (only_digits.group(1))
     digits_with_or = re.match(r'([\d+\s*]+)\|([\d+\s*]+)', value)
     if digits_with_or:
-        return key, ('or', np.array([np.array(v.split(), dtype=int)
-                              for v in digits_with_or.groups()], dtype=int))
+        regex = f'(({digits_with_or.group(1)})|({digits_with_or.group(2)}))'
+        return key, regex
     letters_only = re.match(r'"(\w+)"$', str(value))
     if letters_only:
-        return key, ('letter', letters_only.group(1))
+        return key, letters_only.group(1)
     raise NotImplementedError('wrong rule')
 
 
-def add_regex(rule, regex, rules_dict):
-    name, rule_val = rule
-    if name == 'letters':
-        return rule_val
-    elif name == 'strict':
-        addition = ''
-        for val in rule_val:
-            subrule = rules_dict[val]
-            addition += add_regex(subrule, regex, rules_dict)
-        return addition
-    print(rule)
+def extend_rule(rule, rules_dict):
+    rule = str(rule).replace('\n', '')
+    eights = 0
+    elevens = 0
+    for _ in range(1500):
+        match_iter = re.finditer(r'(\d+)', rule)
+        try:
+            match = next(match_iter)
+            # print(match)
+            # if match.group(0) == '8':
+            #     print('eight')
+            #     eights += 1
+            #     match = next(match_iter)
+            rule = f'{rule[:match.start(0)]} {rules_dict[match.group(0)]} {rule[match.end(0):]}'
+            # print('rule', rule)
+        except StopIteration:
+            break
 
-
-
-def check_pass(dict, text, match_code):
-    pass
+    return rule
 
 
 if __name__ == '__main__':
-    data = read_input_blocks('input_ex')
-    print(data)
-
+    data = read_input_blocks('input')
+    second = False
     rules, checks = data
+
     rules_dict = {}
     for rule in rules:
+        if second and rule == '8: 42':
+            rule = '8: 42 | 42 8'
+        elif second and rule == '11: 42 31':
+            rule = '11: 42 31 | 42 11 31'
         key, value = parse_rule(rule)
         rules_dict.update(**{key: value})
 
-    print(rules_dict)
-    # print(extend_rule(rules_dict['0'], rules_dict))
-    # [rules.update(**{key: value}) for key, value in
-    # print(rules)
-
-    # [[4 1 5]]
-    # if len == 1:
-    #     for i in els:
-    #         text, 4, 1, 5
+    extended = extend_rule(rules_dict['0'], rules_dict)
+    # print(extended)
+    extended = ''.join(extended).replace('[', '(').replace(']', ')').replace(' ', '')
+    print(sum([(re.fullmatch(extended, name) is not None) for name in checks]))
